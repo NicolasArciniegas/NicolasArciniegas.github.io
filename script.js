7cfa28a1c63d9e288ecf9ea231678c88cc37164b
Nicolas Arciniegas <nicolas.arciniegas@outlook.com>
2022-01-09 18:35:01 -0500

    Animaciones y seccion habilidades

--- script.js
+++ script.js
@@ -13,6 +13,38 @@ function cambiarColor() {
         a.style.color = "black";
     }
 }
+function animarCargaBarra (elemento, carga) {
+    let objeto = document.getElementById(elemento);
+    let carga_actual = 0;
+    let intervalo = setInterval(function() {
+        carga_actual += 1;
+        objeto.style.width = carga_actual + "rem";
+        if (carga_actual >= carga) {
+            objeto.style.widht = carga - carga_actual + "rem";
+            clearInterval(intervalo);
+        }
+    }, 50);
+}
+function hacerFadeIn (objeto, tiempo) {
+    objeto.style.opacity = 0;
+    let intervalo = setInterval(function() {
+        let actual = parseFloat(objeto.style.opacity);
+        if (actual >= 1) {
+            clearInterval(intervalo);
+        } else {
+            objeto.style.opacity = actual + 0.1;
+        }
+    }, tiempo);
+}
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 // Inicio escucho de eventos
 var scrolleando = false;
 window.addEventListener('scroll', function(e) {
@@ -21,18 +53,34 @@ window.addEventListener('scroll', function(e) {
 var temporizador = setInterval(reiniciarColor, 700);
 //botones
 window.onload = function() {
+    // Fade in
+    var fadeInFirst = document.getElementsByClassName("fade-in-first");
+    for (elm of fadeInFirst) {
+        setTimeout(
+            hacerFadeIn(elm, 100), 1000
+        );
+        
+    }
+    let obj = document.getElementById("nombre-principal");
+    setTimeout(
+        hacerFadeIn(obj, 200), 5000
+    )
+    // Boton emepzar
     var botonEmpezar = document.getElementById("boton-empezar");
     botonEmpezar.onclick = function() {
         window.scrollTo({
-            top: 541,behavior: 'smooth'
+            top: 570,behavior: 'smooth'
             })
 }
     let elementos = {
         'menu-a-1': 0,
         'menu-a-2': 570,
-        'menu-a-5': 1937,
+        'menu-a-3': 1252,
+        'menu-a-4': 1934,
+        'menu-a-5': 2616,
 
     };
+    // Botones menu
     for (let a in elementos) {
         let btn = document.getElementById(a);
         btn.onclick = function() {
@@ -41,4 +89,23 @@ window.onload = function() {
             });
         }
     }
+    // Fadein barras
+    var idsFadeIn = {
+        'python': 20,
+        'html': 17.5,
+        'css': 12.5,
+        'js': 13,
+    };
+    var tempoCargaBarra = setInterval(
+        function() {
+            if (isInViewport(document.getElementById('python'))) {
+                for (let elm in idsFadeIn) {
+                animarCargaBarra(elm, idsFadeIn[elm]);
+            }
+            console.log("entro");
+            clearInterval(tempoCargaBarra)
+        }
+    }, 500
+    );
+    
 }
